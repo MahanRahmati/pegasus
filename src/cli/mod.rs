0ab527a92486aff54d16c15f9eb58553cdaa3ae3
Mahan <mahan.rahmati@protonmail.com>
2026-02-04 22:08:50 -0500

    feat: add reset config to cli

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -4,14 +4,23 @@
 //! arguments and subcommands. It provides type-safe argument handling and
 //! automatic help generation.
 
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
 #[command(name = "Pegasus")]
 #[command(version = env!("CARGO_PKG_VERSION"))]
 #[command(about = concat!("Pegasus v", env!("CARGO_PKG_VERSION")))]
 pub struct Cli {
+  #[command(subcommand)]
+  pub command: Option<Commands>,
+
   /// Use verbose output
   #[arg(short, long, default_value_t = false, global = true)]
   pub verbose: bool,
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+  /// Reset configuration to default values
+  ResetConfig,
 }
